e62275b8e15f7803d88556d64ac4d50b746ae6a3
wadewaldron <wade.waldron@boldradius.com>
2017-01-12 13:32:16 -0600

    Added a script to validate a student repo by running all it's tests.

---
+++ validateStudentRepo.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -z ${1+x} ]; then
+  echo "validateStudentRepo: Missing Parameter"
+  echo "usage: validateStudentRepo [directory]"
+  exit 1
+fi
+
+startingDir=`pwd`
+studentRepo=$1
+
+GREEN='\033[0;32m'
+RESET='\033[0m' # No Color
+SEPARATOR="##########################################################"
+
+function validateNextExercise {    
+    echo $SEPARATOR
+    if sbt nextExercise 2>&1 | grep "Moved to"; then
+        sbt ";pullSolution;test"
+        return $?
+    else
+        return 1
+    fi
+}
+
+function validateAllExercises {
+    echo $SEPARATOR
+    echo Validating Exercises in $studentRepo
+    cd $studentRepo
+
+    echo $SEPARATOR
+    sbt "gotoExerciseNr 0" 2>&1 | grep "Moved to"
+
+    while validateNextExercise 
+    do
+        echo $SEPARATOR
+        echo -e "[${GREEN}SUCCESS${RESET}] Validated Exercise"
+    done
+    
+    echo $SEPARATOR
+    echo "Validation Completed"
+
+    cd $startingDir
+}
+
+validateAllExercises

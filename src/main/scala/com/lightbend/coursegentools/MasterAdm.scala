5fac3ee43563c65fab8f0b6c368911b5d3923a96
Eric Loots <eric.loots@gmail.com>
2018-01-03 10:51:06 +0100

    Fix duplicate/insert exercise before exercise 0 + reformat for clarity

--- src/main/scala/com/lightbend/coursegentools/MasterAdm.scala
+++ src/main/scala/com/lightbend/coursegentools/MasterAdm.scala
@@ -24,13 +24,22 @@ object MasterAdm {
     val exercises: Vector[String] = getExerciseNames(masterRepo)
     val exerciseNumbers = exercises.map(extractExerciseNr)
 
-    (regenBuildFile, duplicateInsertBefore, deleteExerciseNr, renumberExercises, renumberExercisesBase, renumberExercisesStep) match {
-      case (true, None, None, false, _, _) =>
+    (regenBuildFile,
+     duplicateInsertBefore,
+     deleteExerciseNr,
+     renumberExercises, renumberExercisesBase, renumberExercisesStep) match {
+      case (true,
+            None,
+            None,
+            false, _, _) =>
         createMasterBuildFile(exercises, masterRepo, multiJVM)
 
-      case (false, Some(dibExNr), None, false, _, _) if exerciseNumbers.contains(dibExNr) =>
+      case (false,
+            Some(dibExNr),
+            None,
+            false, _, _) if exerciseNumbers.contains(dibExNr) =>
         val exercise = getExerciseName(exercises, dibExNr).get
-        if (exerciseNumbers.contains(dibExNr - 1)) {
+        if (exerciseNumbers.contains(dibExNr - 1) || dibExNr == 0) {
           duplicateExercise(masterRepo, exercise, dibExNr)
           shiftExercisesUp(masterRepo, exercises, dibExNr, exerciseNumbers)
         } else {
@@ -38,22 +47,34 @@ object MasterAdm {
         }
         createMasterBuildFile(getExerciseNames(masterRepo), masterRepo, multiJVM)
 
-      case (false, Some(dibExNr), None, false, _, _) =>
+      case (false,
+            Some(dibExNr),
+            None,
+            false, _, _) =>
         println(toConsoleRed(s"Duplicate and insert before: no exercise with number $dibExNr"))
         System.exit(-1)
 
-      case (false, None, Some(dibExNr), false, _, _) if exerciseNumbers.contains(dibExNr) =>
+      case (false,
+            None,
+            Some(dibExNr),
+            false, _, _) if exerciseNumbers.contains(dibExNr) =>
         import sbt.io.{IO => sbtio}
         val relativeSourceFolder = new File(masterRepo, config.relativeSourceFolder)
         val exercise = getExerciseName(exercises, dibExNr).get
         sbtio.delete(new File(relativeSourceFolder, exercise))
         createMasterBuildFile(getExerciseNames(masterRepo), masterRepo, multiJVM)
 
-      case (false, None, Some(dibExNr), false, _, _) =>
+      case (false,
+            None,
+            Some(dibExNr),
+            false, _, _) =>
         println(toConsoleRed(s"Delete exercise: no exercise with number $dibExNr"))
         System.exit(-1)
 
-      case (false, None, None, true, offset, step) =>
+      case (false,
+            None,
+            None,
+            true, offset, step) =>
         import sbt.io.{IO => sbtio}
         val relativeSourceFolder = new File(masterRepo, config.relativeSourceFolder)
         val moves = for {

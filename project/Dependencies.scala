46f048d38d362ea6cf2f8d1986d96cf4ea95b9c6
Eric Loots <eric.loots@gmail.com>
2020-04-29 21:38:22 +0200

    Bump dependencies/Remove unused dependency (#80)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,14 @@
 import sbt._
 
 object Version {
-  val scalaVer        = "2.12.3"
-  val scalaParsers    = "1.0.4"
-  val scalaTest       = "3.0.1"
-  val scopt           = "3.7.0"
-  val sbtio           = "1.0.0"
-  val typesafeConfig  = "1.3.1"
+  val scalaVer        = "2.13.2"
+  val scalaTest       = "3.1.1"
+  val scopt           = "3.7.1"
+  val sbtio           = "1.3.4"
+  val typesafeConfig  = "1.4.0"
 }
 
 object Library {
-  val scalaParsers    = "org.scala-lang.modules" %% "scala-parser-combinators" % Version.scalaParsers
   val scalaTest       = "org.scalatest"          %% "scalatest"                % Version.scalaTest
   val scopt           = "com.github.scopt"       %% "scopt"                    % Version.scopt
   val sbtio           = "org.scala-sbt"          %% "io"                       % Version.sbtio
@@ -22,7 +20,6 @@ object Dependencies {
   import Library._
 
   val studentify = List(
-    scalaParsers,
     scopt,
     sbtio,
     typesafeConfig,

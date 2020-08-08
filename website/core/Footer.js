aa082af12422fcc63525cfd5f3493c78deb4daa7
Eric Loots <eric.loots@gmail.com>
2020-08-08 14:32:07 +0200

    Fix broken links in footer in reference pages (#135)
    - Probably caused multi-language support in docusaurus that we
      don't fully understand.
    - Assuming for the time being thatwe only will support docs in
      English.

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -12,8 +12,9 @@ class Footer extends React.Component {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
     const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
+    // const langPart = `${language ? `${language}/` : ''}`;
+    // return `${baseUrl}${docsPart}${langPart}${doc}`;
+    return `${baseUrl}${docsPart}${doc}`;
   }
 
   pageUrl(doc, language) {

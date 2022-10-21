5083ef55c5e0ca1095a858c9caf454bb784bc3c3
Eric Loots <eric.loots@gmail.com>
2022-10-21 12:03:07 +0200

    Update dependencies (#186)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,12 +1,12 @@
 import sbt._
 
 object Version {
-  val scalaVersion = "3.1.1"
-  val scalaTest = "3.2.11"
+  val scalaVersion = "3.2.0"
+  val scalaTest = "3.2.14"
   val scalaCheck = "3.2.9.0"
-  val scopt = "4.0.1"
-  val sbtio = "1.6.0"
-  val typesafeConfig = "1.4.1"
+  val scopt = "4.1.0"
+  val sbtio = "1.7.0"
+  val typesafeConfig = "1.4.2"
 }
 
 object Library {

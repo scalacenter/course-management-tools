e5d97eeb541ed423a1a357b609f1a7df23f91c18
Trevor Burton-McCreadie <trevor@thinkmorestupidless.com>
2022-12-07 10:15:32 +0000

    Added Error Type (#191)
    * Added Error type and some examples of implementations
    
    * Tweak `FailedToExecuteCommand` Error type
    
    Co-authored-by: Eric Loots <eric.loots@toto.com>
    Co-authored-by: Eric Loots <eric.loots@gmail.com>

---
+++ core/src/main/scala/cmt/Error.scala
@@ -0,0 +1,30 @@
+package cmt
+
+sealed trait CmtError {
+  def toDisplayString: String
+}
+
+final case class RequiredOptionIsMissing(option: OptionName) extends CmtError {
+  override def toDisplayString: String =
+    s"missing required option ${option.value}"
+}
+final case class FailedToValidateArgument(option: OptionName, reasons: Seq[ErrorMessage]) extends CmtError {
+  override def toDisplayString: String =
+    s"""Failed to validate arguments:${reasons.foreach(str => s"\n    $str")}"""
+}
+object FailedToValidateArgument:
+  def because(option: String, messages: String*): FailedToValidateArgument =
+    FailedToValidateArgument(OptionName(option), messages.map(ErrorMessage(_)))
+
+final case class FailedToValidateCommandOptions(reasons: List[ErrorMessage]) extends CmtError {
+  override def toDisplayString: String =
+    s"""Failed to validate command options:${reasons.foreach(str => s"\n    $str")}"""
+}
+
+final case class FailedToExecuteCommand(reason: ErrorMessage) extends CmtError {
+  override def toDisplayString: String =
+    s"""Failed to execute command: ${reason.message}"""
+}
+
+case class OptionName(value: String)
+case class ErrorMessage(message: String)

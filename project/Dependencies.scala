60298bad8c57b04bdc9aca0c2ecf927decf30264
Eric Loots <eric.loots@gmail.com>
2023-06-05 14:11:25 +0200

    Enable linting and discarded values warnings
    - Enable linting (as available since Scala 3.3.0)
    - Enable warning about discarded values
    - Bump Scala & sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,7 @@
 import sbt._
 
 object Version {
-//  val scalaVersion = "3.3.0-RC3"
-  lazy val scalaVersion = "3.2.2"
+  lazy val scalaVersion = "3.3.0"
   lazy val scalaTest = "3.2.14"
   lazy val scalaCheck = "3.2.9.0"
   lazy val sbtio = "1.7.0"

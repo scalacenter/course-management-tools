c978ccc40756584ce2b966b67bfc5986a5bfbee6
Eric Loots <eric.loots@gmail.com>
2023-07-19 11:45:19 +0200

    Improve `cmtc install` error messages/remove unused code (#241)
    * Improve `cmtc install` error messages/remove unused code
    
    * Add extra target folder validation on cmta commands
    
    - `cmta linearize` was missing target folder validation
      altogether
    - Added target folder validation on `cmta studentify` and
      `cmta linearize` commands
    
    * Better naming

--- cmta/src/main/scala/com/lunatech/cmt/admin/command/Linearize.scala
+++ cmta/src/main/scala/com/lunatech/cmt/admin/command/Linearize.scala
@@ -6,6 +6,7 @@ import com.lunatech.cmt.{CMTaConfig, CmtError, printResult, toConsoleGreen}
 import com.lunatech.cmt.admin.Domain.{ForceDeleteDestinationDirectory, LinearizeBaseDirectory, MainRepository}
 import com.lunatech.cmt.admin.cli.ArgParsers.{forceDeleteDestinationDirectoryArgParser, linearizeBaseDirectoryArgParser}
 import com.lunatech.cmt.admin.cli.SharedOptions
+import com.lunatech.cmt.admin.validateDestinationFolder
 import com.lunatech.cmt.core.cli.CmtCommand
 import com.lunatech.cmt.core.execution.Executable
 import com.lunatech.cmt.core.validation.Validatable
@@ -30,7 +31,12 @@ object Linearize:
   given Validatable[Linearize.Options] with
     extension (options: Linearize.Options)
       def validated(): Either[CmtError, Linearize.Options] =
-        Right(options)
+        for {
+          mainRepository <- resolveMainRepoPath(options.shared.mainRepository.value)
+          _ <- validateDestinationFolder(
+            mainRepository = mainRepository,
+            destination = options.linearizeBaseDirectory.value)
+        } yield options
   end given
 
   given Executable[Linearize.Options] with

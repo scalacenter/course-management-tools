334f98c71ad2fe55c3a40865bae38784f0a2c220
Lunatech Ops <ops@lunatech.com>
2023-07-26 07:05:00 +0000

    Update scalacheck-1-15 to 3.2.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Version {
   lazy val scalaVersion = "3.3.0"
   lazy val scalaTest = "3.2.16"
-  lazy val scalaCheck = "3.2.11.0"
+  lazy val scalaCheck = "3.2.14.0"
   lazy val sbtio = "1.9.1"
   lazy val typesafeConfig = "1.4.2"
   lazy val caseapp = "2.1.0-M25"
@@ -17,7 +17,7 @@ object Version {
 
 object Library {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest % Test
-  lazy val scalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % Version.scalaCheck % Test
+  lazy val scalaCheck = "org.scalatestplus" %% "scalacheck-1-16" % Version.scalaCheck % Test
   lazy val sbtio = "org.scala-sbt" %% "io" % Version.sbtio
   lazy val typesafeConfig = "com.typesafe" % "config" % Version.typesafeConfig
   lazy val commonsCodec = "commons-codec" % "commons-codec" % "1.16.0"

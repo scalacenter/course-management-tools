59f896f4153afc508ef34a04a7748cb97c98b6a2
Eric Loots <eric.loots@gmail.com>
2023-08-23 18:07:59 +0200

    Fix cmtc install on windows (#285)
    * Fix 'cmtc install' on windows
    
    - Introduce Li Haoyi's 'requests' and 'os' libraries
    - Clean-up build dependencies file
    - Swap file download via scala process API to `requests/os` API
    - Add trimming of output of `git ls-remote` command
    
    * Update native-image config files

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,37 +2,46 @@ import sbt._
 
 object Version {
   lazy val scalaVersion = "3.3.0"
-  lazy val scalaTest = "3.2.16"
-  lazy val scalaCheck = "3.2.14.0"
-  lazy val sbtio = "1.9.1"
-  lazy val typesafeConfig = "1.4.2"
-  lazy val caseapp = "2.1.0-M25"
-  lazy val cats = "2.10.0"
-  lazy val devDirs = "26"
-  lazy val github4s = "0.32.0"
-  lazy val http4s = "0.23.15"
-  lazy val circe = "0.14.5"
-  lazy val circeConfig = "0.8.0"
+  lazy val scalaTestVersion = "3.2.16"
+  lazy val scalaCheckVersion = "3.2.14.0"
+  lazy val sbtioVersion = "1.9.1"
+  lazy val typesafeConfigVersion = "1.4.2"
+  lazy val caseappVersion = "2.1.0-M25"
+  lazy val catsVersion = "2.10.0"
+  lazy val devDirsVersion = "26"
+  lazy val github4sVersion = "0.32.0"
+  lazy val http4sVersion = "0.23.15"
+  lazy val circeVersion = "0.14.5"
+  // lazy val circeConfig = "0.8.0"
+  lazy val LiHaoyiOsLibVersion = "0.9.1"
+  lazy val LiHaoyiRequestLibVersion = "0.8.0"
+  lazy val commonsCodecVersion = "1.16.0"
+
 }
 
 object Library {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest % Test
-  lazy val scalaCheck = "org.scalatestplus" %% "scalacheck-1-16" % Version.scalaCheck % Test
-  lazy val sbtio = "org.scala-sbt" %% "io" % Version.sbtio
-  lazy val typesafeConfig = "com.typesafe" % "config" % Version.typesafeConfig
-  lazy val commonsCodec = "commons-codec" % "commons-codec" % "1.16.0"
-  lazy val caseapp = "com.github.alexarchambault" %% "case-app" % Version.caseapp
-  lazy val cats = "org.typelevel" %% "cats-core" % Version.cats
-  lazy val devDirs = ("dev.dirs" % "directories" % Version.devDirs).withJavadoc()
+
+  import Version._
+
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % Test
+  lazy val scalaCheck = "org.scalatestplus" %% "scalacheck-1-16" % scalaCheckVersion % Test
+  lazy val sbtio = "org.scala-sbt" %% "io" % sbtioVersion
+  lazy val typesafeConfig = "com.typesafe" % "config" % typesafeConfigVersion
+  lazy val commonsCodec = "commons-codec" % "commons-codec" % commonsCodecVersion
+  lazy val caseapp = "com.github.alexarchambault" %% "case-app" % caseappVersion
+  lazy val cats = "org.typelevel" %% "cats-core" % catsVersion
+  lazy val devDirs = ("dev.dirs" % "directories" % devDirsVersion).withJavadoc()
+  lazy val requestLib = "com.lihaoyi" %% "requests" % LiHaoyiRequestLibVersion
+  lazy val osLib = "com.lihaoyi" %% "os-lib" % LiHaoyiOsLibVersion
 
   lazy val http4s = Seq(
     "org.http4s" %% "http4s-dsl",
     "org.http4s" %% "http4s-blaze-server",
     "org.http4s" %% "http4s-blaze-client",
-    "org.http4s" %% "http4s-circe").map(_ % Version.http4s)
+    "org.http4s" %% "http4s-circe").map(_ % http4sVersion)
 
-  lazy val circe = "io.circe" %% "circe-generic" % Version.circe
-  lazy val github4s = "com.47deg" %% "github4s" % Version.github4s
+  lazy val circe = "io.circe" %% "circe-generic" % circeVersion
+  lazy val github4s = "com.47deg" %% "github4s" % github4sVersion
 }
 
 object Dependencies {
@@ -52,6 +61,7 @@ object Dependencies {
     github4s)).map(_.withSources())
   lazy val cmtDependencies =
     List(sbtio, typesafeConfig, scalaTest, scalaCheck, commonsCodec, caseapp, cats).map(_.withSources())
-  lazy val cmtcDependencies = (http4s ++ List(devDirs, circe, github4s, scalaTest, scalaCheck)).map(_.withSources())
+  lazy val cmtcDependencies =
+    (http4s ++ List(devDirs, circe, github4s, scalaTest, scalaCheck, osLib, requestLib)).map(_.withSources())
   lazy val functionalTestDependencies = List(scalaTest, scalaCheck).map(_.withSources())
 }
